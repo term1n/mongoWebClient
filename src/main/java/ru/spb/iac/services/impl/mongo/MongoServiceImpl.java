6a9ac0b5b44772ba978326330e2e53b383e17175
stepa <ismakaev@iac.spb.ru>
2014-10-10 17:52:21 +0400

    MongoServiceImpl developed find operation

--- src/main/java/ru/spb/iac/services/impl/mongo/MongoServiceImpl.java
+++ src/main/java/ru/spb/iac/services/impl/mongo/MongoServiceImpl.java
@@ -1,9 +1,16 @@
 package ru.spb.iac.services.impl.mongo;
 
+import com.mongodb.DBObject;
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import ru.spb.iac.services.MongoService;
+import ru.spb.iac.services.MongoTemplateFactory;
+import ru.spb.iac.services.impl.templates.MongoTemplateFactoryImpl;
 import ru.spb.iac.ui.model.MongoAddress;
 
 import java.util.List;
@@ -15,28 +22,36 @@ import java.util.List;
 @Log4j
 @Service("mongoService")
 public class MongoServiceImpl implements MongoService {
+
+    @Autowired
+    @Qualifier("mongoTemplateFactory")
+    private MongoTemplateFactory mongoTemplateFactory;
+
     @Override
-    public void save() {
+    public void save(MongoAddress mongoAddress, Object obj) {
 
     }
 
     @Override
-    public void update() {
+    public void update(MongoAddress mongoAddress, Update update) {
 
     }
 
     @Override
     public List<Object> find(MongoAddress mongoAddress, Query query) {
-        return null;
+        List<Object> resultLst = null;
+        MongoOperations mongoOperations = mongoTemplateFactory.getOperationsTemplate(mongoAddress);
+        resultLst = mongoOperations.find(query, Object.class, mongoAddress.getCollName());
+        return resultLst;
     }
 
     @Override
-    public void findOne() {
+    public void findOne(MongoAddress mongoAddress, Query query) {
 
     }
 
     @Override
-    public boolean remove() {
+    public boolean remove(MongoAddress mongoAddress, Query query) {
         return false;
     }
 }
